beb30caae095d53cee350351985d238c706eca23
KillyKicks342 <kyleman646@gmail.com>
2021-09-28 21:03:19 +0100

    making of gmail accounts.

--- emailProfiles/gmailaccounts.py
+++ emailProfiles/gmailaccounts.py
@@ -1,6 +1,7 @@
 import random
 import names
 import csv
+import functools
 
 
 class gmailAccounts():
@@ -24,7 +25,7 @@ gmail = gmailAccounts(gmail="@gmail.com")
 outlook = outlookAccounts(outlook="@outlook.com")
 
 
-def accounts():
+def Main():
     emails = input(
         "Please type how many profiles you would like to make...")
     if emails.isdigit() is True:
@@ -33,16 +34,46 @@ def accounts():
         print("Error invalid character/s, make sure you are entering a digit.")
 
 
-with open(f'./end.csv', 'w', newline='') as file:
-    fieldNames = ['first name', 'second name', 'email']
-    writer = csv.DictWriter(file, fieldnames=fieldNames)
-    writer.writeheader()
-    for i in range(accounts()):
-        fullNames = names.get_full_name()
-        name = fullNames.replace(" ", "") + str(random.randint(0, 99))
-        gmailsAcc = name + gmailAccounts.domainNameGmail(gmail)
-        space = fullNames.find(" ")
-        first = fullNames[:space]
-        second = fullNames[space:].replace(" ", "")
-        writer.writerow(
-            {'first name': first, 'second name': second, 'email': gmailsAcc})
+def trackcalls(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        wrapper.has_been_called = True
+        return func(*args, **kwargs)
+    wrapper.has_been_called = False
+    return wrapper
+
+
+@trackcalls
+def example():
+    pass
+
+
+example()
+
+if example.has_been_called:
+    total = [0]
+    for i in range(1):
+        total.reverse()
+        if total[0] == 0:
+            total.append(1)
+        if total[0] != 0:
+            total.reverse()
+            last = total[-1]
+            end = last + 1
+            total.append(end)
+        ending = total[-1]
+        with open(f'./end{ending}.csv', 'w', newline='') as file:
+            fieldNames = ['first name', 'second name', 'email']
+            writer = csv.DictWriter(file, fieldnames=fieldNames)
+            writer.writeheader()
+            for i in range(Main()):
+                fullNames = names.get_full_name()
+                name = fullNames.replace(
+                    " ", "") + str(random.randint(0, 99))
+                gmailsAcc = name + gmailAccounts.domainNameGmail(gmail)
+                space = fullNames.find(" ")
+                first = fullNames[:space]
+                second = fullNames[space:].replace(" ", "")
+                writer.writerow(
+                    {'first name': first, 'second name': second, 'email': gmailsAcc})
+            file.close()
