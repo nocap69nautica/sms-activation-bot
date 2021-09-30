92913b76b0cc20a60d471e5bcabccfdd95a097c4
KillyKicks342 <kyleman646@gmail.com>
2021-09-30 23:46:00 +0100

    Gmail account info generator working.

--- emailProfiles/test.py
+++ emailProfiles/test.py
@@ -1,19 +1,5 @@
-def count(func):
-    def wrapper(*args, **kwargs):
-        wrapper.counter += 1    # executed every time the wrapped function is called
-        return func(*args, **kwargs)
-    wrapper.counter = 0         # executed only once in decorator definition time
-    return wrapper
-
-
-@count
-def func():
-    pass
-
-
-def count():
-    if func:
-        return func.counter
-
-
-count()
+x = range(0, 10)
+foo = []
+for i in x:
+    foo.append(i)
+    print(foo[i])
